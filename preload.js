3fd480295ac896cf4450a23ba2a635c2b1ce63f0
Alexander Flink <alexanderflink93@gmail.com>
2018-05-25 16:45:13 +0200

    Started working on automatically seeking in root timeline

--- preload.js
+++ preload.js
@@ -9,3 +9,11 @@ window.fallback = function(callback) {
 	callback();
 	ipcRenderer.send("capture-screen");
 };
+
+setTimeout(function() {
+	var rootTimeline = TimelineLite.exportRoot();
+	rootTimeline.seek(rootTimeline.duration())
+		.add(function() {
+			ipcRenderer.send("capture-screen");
+		});
+}, 500);
