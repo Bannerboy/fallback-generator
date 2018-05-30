f4dd030c2e65b8d8148c19ef4592cb237b6cf34b
alexanderflink <alexanderflink93@gmail.com>
2018-05-30 17:27:58 +0200

    Fixed bug where callback method wasn't working

--- preload.js
+++ preload.js
@@ -5,37 +5,40 @@ var bannerboy = bannerboy || {};
 
 console.log("PRELOAD SCRIPT");
 
-window.addEventListener("load", () => {
+let fastForwardMethod = localStorage.getItem("fast-forward");
 
-	setTimeout(()=> {
-		function capture() {
-			ipcRenderer.send("capture-screen");
-		}
+// if fast forward method is callback, run immediately before onload
+if (fastForwardMethod === "callback") {
+	window.fallback = function(callback) {
+		callback();
+		capture();
+	};
+} else {
+	window.addEventListener("load", () => {
+		setTimeout(()=> {
+			let seconds = localStorage.getItem("seek-to-time");
+			switch (fastForwardMethod) {
+				case "fast-forward":
+					var rootTimeline = TimelineLite.exportRoot();
+					rootTimeline.timeScale(10);
+					setTimeout(capture, (seconds * 1000) * 0.1);
+					break;
+				case "seek":
+					var rootTimeline = TimelineLite.exportRoot();
+					rootTimeline.seek(rootTimeline.duration());
+					setTimeout(capture, 1);
+					break;
+			}
 
-		let seconds = localStorage.getItem("seek-to-time");
-		switch (localStorage.getItem("fast-forward")) {
-			case "fast-forward":
-				var rootTimeline = TimelineLite.exportRoot();
-				rootTimeline.timeScale(10);
-				setTimeout(capture, (seconds * 1000) * 0.1);
-				break;
-			case "seek":
-				var rootTimeline = TimelineLite.exportRoot();
-				rootTimeline.seek(rootTimeline.duration());
-				setTimeout(capture, 1);
-				break;
+			// if 3000ms has passed, there is probably something wrong. Timeout
+			// setTimeout(() => {
+			// 	ipcRenderer.send("timeout");
+			// }, 10000);
+		}, 100);
+	});
+}
 
-			case "callback":
-				window.fallback = function(callback) {
-					callback();
-					capture();
-				};
-				break;
-		}
+function capture() {
+	ipcRenderer.send("capture-screen");
+}
 
-		// if 3000ms has passed, there is probably something wrong. Timeout
-		// setTimeout(() => {
-		// 	ipcRenderer.send("timeout");
-		// }, 10000);
-	}, 100);
-});
